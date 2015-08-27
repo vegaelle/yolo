cf04785ae4228aaad79b4e44a76f865a3aba7d3a
Damien Nicolas <git@gordon.re>
2015-08-27 14:41:24 +0200

    added attributes support for EventView

--- learning/models.py
+++ learning/models.py
@@ -1,20 +1,29 @@
 from calendar import Calendar
 from datetime import timedelta, date, datetime
+
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.utils.text import slugify
 from polymorphic import PolymorphicModel
 from dateutils import relativedelta
+
 from .validators import colorValidator
 
 
 class EventDay:
 
-    def __init__(self, day):
+    def __init__(self, day, attributes=None):
         if isinstance(day, date):
             self.date = day
         elif isinstance(day, datetime):
             self.date = day.date
         self.events = []
+        if attributes is None:
+            attributes = {}
+        self.attributes = attributes
+
+    def add_attributes(self, attributes):
+        self.attributes.update(attributes)
 
     def append(self, item):
         self.events.append(item)
@@ -37,28 +46,63 @@ class EventDay:
 
 class EventView:
 
-    def __init__(self, events, date_getter, end_getter=None):
+    def __init__(self, events, date_getter, end_getter=None, days=None):
         self.events = events
         self.date_getter = date_getter
         self.end_getter = end_getter
         # self.days = defaultdict(lambda: EventDay())
         self.days = {}
+        if days is not None:
+            self.add_days_properties(days)
+        self.add_events(events)
+
+    def add_events(self, events, date_getter=None, end_getter=None):
+        """
+        adds a list of events to an EventView. Events should be a list of
+        objects. Date_getter and end_getter should be attribute/key names of
+        Date objects in the events, or callables that return date objects.
+        """
+        if date_getter is None:
+            date_getter = self.date_getter
+        if end_getter is None:
+            end_getter = self.end_getter
+
         for event in events:
-            event_date = self.get_date_info(event, date_getter)
-            if event_date not in self.days:
-                self.days[event_date] = EventDay(event_date)
-            if end_getter is not None:
-                event_end = self.get_date_info(event, end_getter)
-            if event_end > event_date:
-                end_date = event_end
-                cur_tmp_date = event_date
-                while cur_tmp_date <= end_date:
-                    if cur_tmp_date not in self.days:
-                        self.days[cur_tmp_date] = EventDay(cur_tmp_date)
-                    self.days[cur_tmp_date].append(event)
-                    cur_tmp_date = cur_tmp_date + timedelta(days=1)
+            self.add_event(event, date_getter, end_getter)
+
+    def add_event(self, event, date_getter, end_getter=None):
+        """
+        adds an event to an EventView. It can be any kind of object.
+        Date_getter and end_getter should be attribute/key names of
+        Date objects in the event, or callables that return date object.
+        """
+        event_date = self.get_date_info(event, date_getter)
+        if event_date not in self.days:
+            self.days[event_date] = EventDay(event_date)
+        if end_getter is not None:
+            event_end = self.get_date_info(event, end_getter)
+        if event_end > event_date:
+            end_date = event_end
+            cur_tmp_date = event_date
+            while cur_tmp_date <= end_date:
+                if cur_tmp_date not in self.days:
+                    self.days[cur_tmp_date] = EventDay(cur_tmp_date)
+                self.days[cur_tmp_date].append(event)
+                cur_tmp_date = cur_tmp_date + timedelta(days=1)
+        else:
+            self.days[event_date].append(event)
+
+    def add_days_properties(self, days):
+        """
+        adds properties to a day object.
+        The 'days' argument must be a dict, in which keys are date objects,
+        and values are dicts of properties.
+        """
+        for day, attributes in days.items():
+            if day in self.days:
+                self.days[day].add_attributes(attributes)
             else:
-                self.days[event_date].append(event)
+                self.days[day] = EventDay(day, attributes)
 
     def get_date_info(self, event, getter):
         """
@@ -78,9 +122,18 @@ class EventView:
                 except TypeError:
                     raise TypeError('getter could not apply to event')
 
+        if isinstance(value, datetime):
+            value = value.date
+        elif isinstance(value, date):
+            pass
+        else:
+            raise ValueError('{} should be a date'.format(value))
         return value
 
     def calendar(self):
+        """
+        Renders the view as a list of months.
+        """
         if len(self.days) == 0:
             raise ValueError('Calendar rendering is not permitted without '
                              'events.')
@@ -98,16 +151,9 @@ class EventView:
                     if day.weekday() not in [5, 6]:
                         month_view[i].append([])
                         if day in self.days:
-                            events = self.days[day]
-                            # print('Adding {ev}'.format(ev=events))
-                            # removing to counter the already-removed events
-                            daily_events_count = 0
-                            for ev in events:
-                                if self.get_date_info(ev, self.date_getter) == \
-                                        events.date:
-                                    daily_events_count += 1
+                            events, daily_events_count = \
+                                self._extract_day_events(day)
                             remaining_events -= daily_events_count
-                            # print('Remaining events: {}'.format(remaining_events))
                             month_view[i][j] = events
                         else:
                             month_view[i][j] = EventDay(day)
@@ -115,6 +161,16 @@ class EventView:
                            'dates': month_view})
             current_day = current_day + relativedelta(months=1)
         return months
+
+    def _extract_day_events(self, day):
+        events = self.days[day]
+        # removing to counter the already-removed events
+        daily_events_count = 0
+        for ev in events:
+            if self.get_date_info(ev, self.date_getter) == \
+                    events.date:
+                daily_events_count += 1
+        return (events, daily_events_count)
 
 
 def get_event_calendar(year, month, events=[]):
@@ -157,6 +213,12 @@ class Tag(models.Model):
 
     class Meta:
         verbose_name = 'tag'
+
+    def to_css(self):
+        """
+        Returns the slugged name, for CSS class integration
+        """
+        return slugify(self.name)
 
 
 # Resource models
@@ -308,23 +370,9 @@ class Promotion(models.Model):
         """
         ev = EventView(self.courses.all(), lambda c: c.begin.date(),
                        lambda c: c.end.date())
+        properties = {day.day: day.properties() for day in self.days.all()}
+        ev.add_days_properties(properties)
         return ev.calendar()
-        calendars = []
-        cal = None
-        cur_month = (None, None)
-        for day in self.days.all():
-            month = (day.day.year, day.day.month)
-            if month != cur_month:  # new month
-                if cal is not None:
-                    calendars.append(cal)
-                cal = get_event_calendar(*month)
-                cur_month = month
-            day_pos = ((day.day.day - 1) // 7 + 1, day.day.weekday())
-            cal[day_pos[0]][day_pos[1]][1].extend(day.intervals.all())
-        if cal is not None:
-            calendars.append(cal)
-
-        return calendars
 
     class Meta:
         verbose_name = 'promotion'
@@ -337,6 +385,15 @@ class DayAttribution(models.Model):
                                  related_name='days')
     day = models.DateField(verbose_name='jour')
     tag = models.ForeignKey('Tag', verbose_name='tag', related_name='days')
+
+    def properties(self):
+        """
+        Returns a dict of properties for the current day.
+        """
+        props = {}
+        props['assigned'] = self.assigned
+        props['tag'] = self.tag
+        return props
 
     def __str__(self):
         return '{promotion} : {day}'.format(promotion=self.promotion.name,
